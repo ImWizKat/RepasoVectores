b43b36e5b685328088487d7e0a23cb8fddbb4b9b
danie <danie@DESKTOP-794MJ50>
2021-09-18 15:49:32 +0200

    Agregar archivos de proyecto.

---
+++ RepasoVectores/RepasoVectores/Form1.Designer.cs
@@ -0,0 +1,398 @@
+﻿
+namespace RepasoVectores
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button13 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox6_2 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12_2 = new System.Windows.Forms.TextBox();
+            this.textBox13_2 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(127, 23);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "Ejercicio 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(170, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(122, 23);
+            this.button2.TabIndex = 15;
+            this.button2.Text = "Ejercicio 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(324, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(128, 23);
+            this.button3.TabIndex = 16;
+            this.button3.Text = "Ejercicio 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(475, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(128, 23);
+            this.button4.TabIndex = 17;
+            this.button4.Text = "Ejercicio 4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(623, 12);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(128, 23);
+            this.button5.TabIndex = 18;
+            this.button5.Text = "Ejercicio 5";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(625, 224);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(128, 23);
+            this.button10.TabIndex = 23;
+            this.button10.Text = "Ejercicio 10";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(477, 225);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(128, 23);
+            this.button9.TabIndex = 22;
+            this.button9.Text = "Ejercicio 9";
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(324, 224);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(128, 23);
+            this.button8.TabIndex = 21;
+            this.button8.Text = "Ejercicio 8";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(170, 224);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(122, 23);
+            this.button7.TabIndex = 20;
+            this.button7.Text = "Ejercicio 7";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 224);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(127, 23);
+            this.button6.TabIndex = 19;
+            this.button6.Text = "Ejercicio 6";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button13
+            // 
+            this.button13.Location = new System.Drawing.Point(324, 435);
+            this.button13.Name = "button13";
+            this.button13.Size = new System.Drawing.Size(128, 23);
+            this.button13.TabIndex = 26;
+            this.button13.Text = "Ejercicio 13";
+            this.button13.UseVisualStyleBackColor = true;
+            this.button13.Click += new System.EventHandler(this.button13_Click);
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(170, 435);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(122, 23);
+            this.button12.TabIndex = 25;
+            this.button12.Text = "Ejercicio 12";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(12, 436);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(127, 23);
+            this.button11.TabIndex = 24;
+            this.button11.Text = "Ejercicio 11";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 56);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(127, 132);
+            this.textBox1.TabIndex = 29;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(170, 56);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(122, 132);
+            this.textBox2.TabIndex = 30;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(324, 56);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(128, 132);
+            this.textBox3.TabIndex = 31;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(475, 56);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(128, 132);
+            this.textBox4.TabIndex = 32;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(623, 56);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox5.Size = new System.Drawing.Size(128, 132);
+            this.textBox5.TabIndex = 33;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(12, 269);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(127, 20);
+            this.textBox6.TabIndex = 34;
+            // 
+            // textBox6_2
+            // 
+            this.textBox6_2.Location = new System.Drawing.Point(12, 295);
+            this.textBox6_2.Multiline = true;
+            this.textBox6_2.Name = "textBox6_2";
+            this.textBox6_2.Size = new System.Drawing.Size(127, 106);
+            this.textBox6_2.TabIndex = 35;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(170, 269);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox7.Size = new System.Drawing.Size(122, 132);
+            this.textBox7.TabIndex = 36;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(324, 269);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox8.Size = new System.Drawing.Size(128, 132);
+            this.textBox8.TabIndex = 37;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(475, 269);
+            this.textBox9.Multiline = true;
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox9.Size = new System.Drawing.Size(128, 132);
+            this.textBox9.TabIndex = 38;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(623, 269);
+            this.textBox10.Multiline = true;
+            this.textBox10.Name = "textBox10";
+            this.textBox10.Size = new System.Drawing.Size(128, 132);
+            this.textBox10.TabIndex = 39;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(12, 480);
+            this.textBox11.Multiline = true;
+            this.textBox11.Name = "textBox11";
+            this.textBox11.Size = new System.Drawing.Size(127, 132);
+            this.textBox11.TabIndex = 41;
+            // 
+            // textBox12_2
+            // 
+            this.textBox12_2.Location = new System.Drawing.Point(170, 506);
+            this.textBox12_2.Multiline = true;
+            this.textBox12_2.Name = "textBox12_2";
+            this.textBox12_2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox12_2.Size = new System.Drawing.Size(127, 106);
+            this.textBox12_2.TabIndex = 42;
+            // 
+            // textBox13_2
+            // 
+            this.textBox13_2.Location = new System.Drawing.Point(324, 506);
+            this.textBox13_2.Multiline = true;
+            this.textBox13_2.Name = "textBox13_2";
+            this.textBox13_2.Size = new System.Drawing.Size(128, 106);
+            this.textBox13_2.TabIndex = 43;
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(170, 480);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.Size = new System.Drawing.Size(127, 20);
+            this.textBox12.TabIndex = 47;
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(325, 480);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.Size = new System.Drawing.Size(127, 20);
+            this.textBox13.TabIndex = 48;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(764, 622);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox13_2);
+            this.Controls.Add(this.textBox12_2);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6_2);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button13);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button13;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox6_2;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox12_2;
+        private System.Windows.Forms.TextBox textBox13_2;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+    }
+}
+
---
+++ RepasoVectores/RepasoVectores/Form1.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace RepasoVectores
+{
+    public partial class Form1 : Form
+    {
+        //creamos los arrays que se usan en distintos métodos
+        static int[] array = new int[50];
+        static int[] arraySinReps = new int[50];
+        //también el random
+        static Random aleatorio = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();            
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox2.Text = "(Usa el array generado en el Ej 1)";
+            textBox3.Text = "(Usa el array generado en el Ej 1)";
+            textBox4.Text = "(Usa el array generado en el Ej 1)";
+            textBox5.Text = "(Usa el array generado en el Ej 1)";
+            textBox6_2.Text = "(Usa el array generado en el Ej 1)";
+            textBox7.Text = "(Usa el array generado en el Ej 1)";
+            textBox8.Text = "(Usa el array generado en el Ej 1)\r\nInvierte los números repetidos para poder ver bien qué números se repiten";
+            textBox9.Text = "Ordena el array antes de mostrarlo para que sea más fácil ver si funciona";
+            textBox10.Text = "(Usa el array generado en el Ej 9)";
+            textBox11.Text = "(Usa el array generado en el Ej 9)";
+            textBox12_2.Text = "(Usa el array generado en el Ej 1)\r\nInvierte los números que han sido cambiados para verlos más fácilmente";
+            textBox13_2.Text = "(Usa el array generado en el Ej 1)";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {                     
+            //rellenar el array
+            for (int i = 0; i < array.Length; i++) { 
+                array[i] = aleatorio.Next(1,100);
+            }
+
+            //mostrarlo en el textbox
+            String mostrar = String.Join("\r\n", array);
+
+            textBox1.Text = mostrar;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            String mostrar = String.Join("\r\n",("Número más bajo: " + array.Min()), ("Número más alto: " + array.Max()));
+            textBox2.Text = mostrar;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox3.Text = String.Join("\r\n", ("Número en la posición 20: " + array[19]),("Número en la posición 30: "+ array[29]));
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = "La media es: " + array.Average();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int[] arrayEj5 = array;
+            Array.Sort(arrayEj5);
+            textBox5.Text= String.Join("\r\n", arrayEj5);
+
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (textBox6.Text.Equals("")) textBox6_2.Text = "Introduce el número a buscar y vuelve a pulsar el botón";
+
+            else {
+                int numUsuario = Convert.ToInt32(textBox6.Text);
+                int contador = 0;
+                for (int i = 0; i < array.Length; i++) {
+                    if (numUsuario == array[i]) contador++;
+                }
+                textBox6_2.Text = String.Join("\r\n", ("Has buscado: " + numUsuario), ("Aparece: " + contador + " veces."));
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int[] arrayEj7 = array;
+            Array.Reverse(arrayEj7);
+            textBox7.Text = String.Join("\r\n", arrayEj7);
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            int[] arrayEj8 = array;
+            //array de números que vayan saliendo para no repetir
+            ArrayList nums = new ArrayList();
+
+            for (int i = 0; i < arrayEj8.Length; i++) {
+                if (!nums.Contains(arrayEj8[i])) {
+                    nums.Add(arrayEj8[i]);
+                }
+                else arrayEj8[i] = -arrayEj8[i];
+            }
+            textBox8.Text= String.Join("\r\n", arrayEj8);
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            //mismo que antes, creamos un array dinámico para ir guardando los número que ya están para no repetir
+            ArrayList nums = new ArrayList();
+
+            for (int i = 0; i < arraySinReps.Length; i++){
+                do{
+                    arraySinReps[i] = aleatorio.Next(1, 100);                    
+                }
+                while (nums.Contains(arraySinReps[i]));
+                nums.Add(arraySinReps[i]);
+            }
+
+            Array.Sort(arraySinReps);
+            textBox9.Text = String.Join("\r\n", arraySinReps);
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            int mayor = 0;
+            int menor = 0;
+            for (int i = 0; i < arraySinReps.Length; i++) {
+                if (arraySinReps[i] <= 25) menor++;
+                if (arraySinReps[i] > 25) mayor++;
+            }
+            textBox10.Text = String.Join("\r\n", ("Menores o iguales a 25: " + menor),("Mayores de 25: " + mayor));
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            int [] arrayEj11 = arraySinReps;
+            Array.Sort(arrayEj11);
+            Array.Reverse(arrayEj11);
+            textBox11.Text = String.Join("\r\n", arrayEj11);
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            if (textBox12.Text.Equals("")) textBox12_2.Text = "Introduce un número y vuelve a pulsar el botón";
+
+            else
+            {
+                int numUsuario = Convert.ToInt32(textBox12.Text);
+
+                if (!array.Contains(numUsuario))
+                {
+                    textBox12_2.Text = "Ese número no está en el array";
+                }
+
+                else
+                {
+                    for (int i = 0; i < array.Length; i++)
+                    {
+                        if (numUsuario == array[i]) {
+                            int numAux = 0;
+                            do {
+                                numAux= aleatorio.Next(1, 100);
+                            }
+                            while (array.Contains(numAux));
+                            array[i] = -numAux;
+                        }
+                    }
+                    textBox12_2.Text = String.Join("\r\n", array);
+                }
+            }
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            if (textBox13.Text.Equals("")) textBox13_2.Text = "Introduce un número y vuelve a pulsar el botón";
+            else
+            {
+                int numUsuario = Convert.ToInt32(textBox13.Text);
+                int mayor = 0;
+                int menor = 0;
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] <= numUsuario) menor++;
+                    if (array[i] > numUsuario) mayor++;
+                }
+                textBox13_2.Text = String.Join("\r\n", ("Menores o iguales a " + numUsuario +  " : " + menor), ("Mayores de " + numUsuario + " : " + mayor));
+            }
+
+
+
+            }
+
+    }
+}
